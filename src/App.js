69bdee2dfde2cb49b077833b0cb3b6f7feef0778
IsraelDax <israel.caxile@gmail.com>
2020-07-09 19:14:56 -0300

    feat: add/remove repos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("/repositories").then(
+      resp => setRepositories(resp.data)
+    )
+  }, [])
+
+
   async function handleAddRepository() {
-    // TODO
+    try {
+      const resp = await api.post("/repositories", {
+        title: "Repository " + Date.now(),
+        url: "http://gol.com.br",
+        techs: ["PHP", "Node", "JS"]
+      });
+
+      setRepositories([...repositories, resp.data]);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete("/repositories/" + id)
+    setRepositories(repositories.filter(rep => rep.id !== id))
   }
+
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map((repo, i) => (
+          <li key={repo.id} >
+            {repo.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
